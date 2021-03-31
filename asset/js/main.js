ae8957fe1b7ffe78a27c514340389ecdb9edef8f
david <david@miratechnologiesng.com>
2021-03-31 11:40:08 +0100

    progress

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,3 +1,17 @@
 window.onload = () => {
     console.log('object');
 }
+
+function submit() {
+    const firstname = document.getElementById('firstname');
+    const lastname = document.getElementById('lastname');
+    const phoneNumber = document.getElementById('firstname');
+    const gender = document.getElementById('gender');
+    const address = document.getElementById('address');
+
+    let error = 0;
+
+    
+
+
+}
